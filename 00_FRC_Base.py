ce23318305719898df71285ac59052fe020a3638
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2024-02-19 13:12:30 +1300

    Added num checker and color text functions

---
+++ 00_FRC_Base.py
@@ -0,0 +1,78 @@
+# Functions
+# Lets you change color of printed text easily
+def color_text(text, color):
+    # Code was found using chatGpt using prompt
+    # "Python function that allows me to change the text color"
+    # Code was changed a bit as some parts were unneeded
+
+    # list of colors
+    colors = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'blue': '\033[94m',
+        'magenta': '\033[95m',
+        'cyan': '\033[96m',
+        'white': '\033[97m',
+    }
+
+    # Prints text in specified color
+    print(f"{colors[color]}{text}\033[0m")
+
+
+# checks users enter an integer / float between a low and
+# high number and allows 'xxx'
+def num_check(question, num_type, low=None, high=None):
+    # Used ChatGPT to allow the use of the letter 'x' used the prompt bellow
+    # Make that function allow the letter 'x' to be used
+
+    situation = ''
+    if low is not None and high is not None:
+        situation = "both"
+    elif low is not None and high is None:
+        situation = "low only"
+
+    while True:
+        try:
+            if num_type == "int":
+                # Ask the question
+                response = input(question)
+
+                # Check if response is 'x'
+                if response.lower() == 'xxx':
+                    return response
+
+                # Convert the response to an integer
+                response = int(response)
+            else:
+                # Ask the question
+                response = input(question)
+
+                # Check if response is 'xxx'
+                if response.lower() == 'xxx':
+                    return response
+
+                # Convert the response to a float
+                response = float(response)
+
+            # Checks input is not too high or
+            # too low if both upper and lower bounds are specified
+            if situation == "both":
+                if response < low or response > high:
+                    color_text(f"Please enter a number between {low} and {high}", 'red')
+                    continue
+
+            # Checks input is not too low
+            elif situation == "low only":
+                if response < low:
+                    color_text(f"Please enter a number that is more than {low}", 'red')
+                    continue
+
+            return response
+
+        except ValueError:
+            color_text("Please enter an integer or 'xxx'", 'red')
+            continue
+
+
+# Main Routine
